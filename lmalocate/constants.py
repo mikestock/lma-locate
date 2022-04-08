702e3eb3edfbf56668f22f33b41901e4a33bec2d
Michael Stock <mstock@earthnetworks.com>
2022-04-08 06:56:30 -0500

    Some fundametal constants we're going to need

---
+++ lmalocate/constants.py
@@ -0,0 +1,17 @@
+"""
+Fundamental constants.  I don't expect these guys to change anytime soon
+"""
+
+###
+# The Speed of Light in handy units
+Cms = 299792.458   #speed of light, m/ms
+Cus = 299.792458   #speed of light, m/us
+Cns = 0.299792458  #speed of light, m/ns
+
+###
+# Earth Radius Constants
+Re  = 6371000 #meters (mean Earth radius)
+Rpole = 6356752.  #meters (polar Earth radius)
+Requa = 6378137.  #meters (equatorial Earth radius)
+Eflattening = 0.0033528599338647    #used in some eliptical calculations, (1-f) = Requa/Rpole
+Earc  = 111195.  #meters/degree (Earth mean arclength)
