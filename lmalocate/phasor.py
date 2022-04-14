5fc4513ad02a4c03e5b9c32b9a4892d9b0776b54
Michael Stock <mstock@earthnetworks.com>
2022-04-14 06:40:20 -0500

    Created phasor modual (name TBD)

---
+++ lmalocate/phasor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+#
+"""
+Name TBD
+Takes input data, phases together, and searches for initial-guesses in vicinity of phase center
+"""
+
+import os, sys, time, warnings
+import numpy as np
+#TODO - change to relative imports
+from common import *
+from constants import *
