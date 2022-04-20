a4c804ed5f8cf92b374abee3573a32a5684a6dd2
Michael Stock <michael.stock@noaa.gov>
2022-04-20 18:52:35 -0500

    updated setup with new naming

--- lmalocate/cdistance_setup.py
+++ lmalocate/cdistance_setup.py
@@ -11,7 +11,7 @@ from Cython.Distutils import build_ext
   
 import numpy
   
-ext = Extension("distance", ["distance.pyx"], 
+ext = Extension("cdistance", ["cdistance.pyx"], 
     include_dirs = [numpy.get_include()])
   
 setup(ext_modules=[ext], cmdclass = {'build_ext': build_ext})
