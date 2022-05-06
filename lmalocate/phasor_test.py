56388438ffe0f7483123d93c6782c3cbec10e7a2
Michael Stock <michael.stock@noaa.gov>
2022-05-06 09:10:02 -0500

    crude implementation of toa and tdoa location

--- lmalocate/phasor_test.py
+++ lmalocate/phasor_test.py
@@ -2,6 +2,8 @@ import raw_io
 import phasor
 import glob
 import numpy as np
+from common import *
+from scipy import optimize
 
 """
 This is a script used to test some/most features of the 
@@ -28,8 +30,39 @@ for inPath in inLmaPaths:
     if epoch < 0 or lmaFile.startEpoch < epoch: 
         epoch = lmaFile.startEpoch
 
+def tdoa_resid( x, data ):
+    geodetic = (x[0],x[1],7000)
+    
+    
+    #distance to the reference station doesn't change, 
+    #so we can just calculate it once
+    D0 = distance.vincenty( geodetic, data[0][1:] )
+    D0 = distance.distance3d( D0, 7000-data[0,3] )
+    t0 = data[0,0]
+    
+    i = 1
+    resid = []
+    while i < len(data):
+        Di = distance.vincenty( geodetic, data[i][1:] )
+        Di = distance.distance3d( Di, 7000-data[i,3] )
+        ti = data[i,0]
+
+        resid.append( D0-Di + Cns*(t0-ti) )
+        i += 1
+    return np.array(resid)
+
+def toa_resid( x, data ):
+    # print(x)
+    resid = []
+    for pk in data:
+        D = distance.euclidean( x[1:], pk[1:] )
+        r = x[0]+D/Cns - pk[0]
+        resid.append( r )
+    return np.array( resid )
+
 #loop over epochs, and make groups of frames to initialize Phasor objects with
 epoch -=1
+centers = []
 for iLoop in range( 10 ):
     epoch += 1
     frames = {}
@@ -58,7 +91,48 @@ for iLoop in range( 10 ):
         # print ( '%9i'%np.sqrt((s.calc_residual()**2).sum()) )
 
     solutions.sort()
+    
     for s in solutions:
-        print (s)
+        data = []
+        for pk in s.selectedPeaks:
+            sensorId = chr( pk[1] )
+            nano     = pk[2]
+            lat,lon,alt = s.loc.sensors[ sensorId ].geodetic 
+            x,y,z       = s.loc.sensors[ sensorId ].cartesian 
+            data.append( [nano, x, y, z] ) 
+        data = np.array( data )
+        x0 = s.nano, s.cartesian[0], s.cartesian[1], s.cartesian[2]
+        geodetic0 = xyz2latlonalt( *s.cartesian )
+        try:
+            sol  = optimize.root( toa_resid, x0, args=(data,), method='lm' )
+            x1 = sol.x
+            r = toa_resid( x1, data)
+            r = np.sqrt( (r**2).mean() )    #rms residual, in ns
+            geodetic1 = xyz2latlonalt( *x1[1:] )
+            if sol.success:
+                D = distance.vincenty( geodetic0, geodetic1 )
+                if D < 250000 and r < 10000:
+                    centers.append( (geodetic1[0], geodetic1[1], r ) )
+            print (s, repr(sol.success).rjust(5),  '%5.1f, %6.1f, %7i'%xyz2latlonalt( *sol.x[1:] ), int(r) )
+
+        # x0   = np.array( xyz2latlonalt( *s.cartesian )[:2] )
+        # try:
+        #     sol  = optimize.root( tdoa_resid, x0, args=(data,), method='lm' )
+           
+        #     if sol.success:
+        #         x = sol.x
+        #         geodetic1 = [x0[0], x0[1], 0 ]
+        #         geodetic2 = [x[0], x[1], 0 ]
+        #         D=distance.vincenty( geodetic1, geodetic2 )
+        #         if D < 250000:
+        #             r = tdoa_resid( x, data)
+        #             r = np.sqrt( (r**2).mean() )    #rms residual, in ns
+        #             if r < 10000:
+        #                 centers.append( (x[0], x[1], r ) )
+        #             print (s, sol.success, sol.x, r )
+        except:
+            #Vincenti seems to sometimes have issues, likely when the optimizer asks for distances from lat/lon out of bounds for the planet
+            # print( s, 'something went wrong' )
+            pass
     
 
